8d55548d210dec6305e9d64c0a5cb00cc90711e3
jooyyoo <aliceyun9@naver.com>
2023-04-14 23:41:49 +0900

    [solved] 8911 거북이 : 오타,B 일때는 빼줘야함

--- 01-Implementation/jooyoung/8911.py
+++ 01-Implementation/jooyoung/8911.py
@@ -1,5 +1,5 @@
-dx = (0, 0, -1, -1)  # 상 하 좌 우
-dy = (1, -1, 0, 0)
+dx = (0, -1, 0, 1)  # 상 좌 하 우
+dy = (1, 0, -1, 0)
 
 N = int(input())
 
@@ -9,24 +9,23 @@ for test_case in range(N):
     mx = my = Mx = My = 0
 
     route = list(input().strip())
-    print(route)
 
     for i in route:
         if i == "F":
             x += dx[direction]
             y += dy[direction]
         elif i == "B":
-            x += dx[direction]
-            y += dy[direction]
+            x -= dx[direction]
+            y -= dy[direction]
         elif i == "L":
             direction = (direction + 1) % 4
         elif i == "R":
             direction = (direction + 3) % 4
 
         mx = min(mx, x)
-        my = min(my, x)
+        my = min(my, y)
         Mx = max(Mx, x)
-        My = max(My, x)
+        My = max(My, y)
 
     answer = abs(My-my) * abs(Mx-mx)
     print(answer)
