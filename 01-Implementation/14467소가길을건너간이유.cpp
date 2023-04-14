99b584db6fb9502f7abeffb12eaa166a1925d46d
ljh0608 <m98m@catholic.ac.kr>
2023-04-14 23:41:22 +0900

    14467소가길을건너간이유

---
+++ 01-Implementation/14467소가길을건너간이유.cpp
@@ -0,0 +1,52 @@
+﻿#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<int, int>> p;
+//vector<pair<int, int>> memo;
+int memo[1001];
+int N;
+int cow_num, pos;
+
+int res = 0;
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> cow_num >> pos;
+		p.push_back(make_pair(cow_num, pos));
+	}
+	for (int i = 0; i <=10; i++) {
+	
+		memo[i] = 2;
+	}
+	
+	for (int i = 0; i < N; i++) {
+		int temp = p[i].first;
+
+		if (memo[temp - 1] == 2) {
+			memo[temp - 1] = p[i].second;
+			continue;
+		}
+		else if (memo[temp - 1] != p[i].second) {
+			res++;
+			memo[temp - 1] = p[i].second;
+			continue;
+		}
+		else {
+			continue;
+		}
+
+	}
+
+	cout << res;
+	return 0;
+}
+
+
+
