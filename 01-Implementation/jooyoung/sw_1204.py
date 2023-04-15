798b2880851a1d2c8bffec06cebb220b326190fa
jooyyoo <aliceyun9@naver.com>
2023-04-15 16:04:53 +0900

    [solved] 주석

--- 01-Implementation/jooyoung/sw_1204.py
+++ 01-Implementation/jooyoung/sw_1204.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open("../../../../sds sw/sw samsung/input_1204.txt", "r")
+sys.stdin = open("input_1204.txt", "r")
 
 
 T = int(input())
@@ -17,18 +17,6 @@ for test_case in range(1, T + 1):
         if f[grade] >= f[max_count]:
             max_count = grade
     print(max_count)
-
-
-    # for i in range(len(arr)):
-    #     val = arr.count(arr[i])
-    #     if val >= max_count:
-    #         max_count = val
-    #         if arr[i - 1] >= arr[i]:
-    #             max_index = i-1
-    #         else:
-    #             max_index = i
-    #
-    # print(arr[max_index])
 
 
 
