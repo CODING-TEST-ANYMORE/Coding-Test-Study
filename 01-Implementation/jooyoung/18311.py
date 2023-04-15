ea14ca89e5233fb678bbe18c73abd674175f8fce
jooyyoo <aliceyun9@naver.com>
2023-04-15 16:03:10 +0900

    [solved] 1811 왕복

--- 01-Implementation/jooyoung/18311.py
+++ 01-Implementation/jooyoung/18311.py
@@ -3,21 +3,21 @@ N, K = map(int, input().split())
 lengths = list(map(int, input().split()))
 real_len = [0]
 
-for i in range(len(lengths)):
-    real_len.append(real_len[i] + lengths[i])
+for j in range(len(lengths)):
+    real_len.append(real_len[j] + lengths[j])
 
 if K > real_len[-1]:
     K = K - real_len[-1]
-    i = -1
-    track = lengths[i]  # 5
+    j = -1
+    track = lengths[j]  # 5
     while True:
         if K > track:
-            i = i - 1
-            track += lengths[i]
+            j = j - 1
+            track += lengths[j]
         else:
-            print(i + N + 1)
+            print(j + N + 1)
             break
 else:
-    for i in range(len(real_len)):
-        if real_len[i] <= K < real_len[i + 1]:
-            print(i + 1)
+    for j in range(len(real_len)):
+        if real_len[j] <= K < real_len[j + 1]:
+            print(j + 1)
