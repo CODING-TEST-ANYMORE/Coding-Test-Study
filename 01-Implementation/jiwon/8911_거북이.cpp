cf0fd63b6697ba30bfbb9e81150090602e141a02
shinzz410 <103318297+shinzz410@users.noreply.github.com>
2023-04-18 00:07:16 +0900

    Update 8911_거북이.cpp

--- 01-Implementation/jiwon/8911_거북이.cpp
+++ 01-Implementation/jiwon/8911_거북이.cpp
@@ -2,9 +2,7 @@
 //  main.cpp
 //  baekjoon
 //
-//  Created by 신지원 on 2023/04/14.
-//
-// 답은 잘 나오는데 자꾸 틀렸대요 ㅠㅠ 도대체 왜 틀렸을까요? 다섯시간 동안 고쳤어요 ㅠㅠ
+//  Created by 신지원 on 2023/04/10.
 //
 
 #include <iostream>
@@ -16,87 +14,58 @@
 #include <queue>
 
 using namespace std;
-
-int n,m,x,y,k;
-vector<int> dice(7);
-//[1] [2] [3] [4] [5] [6]
-int MAP[20][20];
-
-void go(int num){
-    
-    if(num == 1){
-        //east
-        //array 배열로 할라니까 에러가 난다 할당할 수 없다나 ...
-        dice = {0, dice[4], dice[2], dice[1], dice[6], dice[5], dice[3]};
-    }
-    if(num == 2){
-        //west
-        dice = {0, dice[3], dice[2], dice[6], dice[1], dice[5], dice[4]};
-    }
-    if(num == 3){
-        //book
-        dice = {0, dice[5], dice[1], dice[3], dice[4], dice[6], dice[2]};
-    }
-    if(num == 4){
-        //nam
-        dice = {0, dice[2], dice[6], dice[3], dice[4], dice[1], dice[5]};
-    }
-    
-    if(MAP[y][x] == 0) MAP[y][x] = dice[6];
-    else dice[6] = MAP[y][x];
-    
-    cout << dice[1] << "\n";
-    
-}
-
-
 int main(int argc, const char * argv[]) {
     ios_base::sync_with_stdio(0);cin.tie(0);
     
-    cin >> n >> m >> x >> y >> k;
+    int T;
+    cin >> T;
     
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cin >> MAP[i][j];
+    for(int i=0;i<T;i++){
+        string s;
+        cin >> s;
+        
+        int xy[2] = {0, }; //x,y
+        int maxx[4] = {0, }; //xmax,ymin,xmin,ymax;
+        int dir = 0; //up, left, down, right;
+        
+        for(int i=0;i<s.length();i++){
+            
+            if(s[i] == 'L' || s[i] == 'R'){
+                if(s[i] == 'L') dir = (dir+1) % 4;
+                if(s[i] == 'R') dir = (dir+3) % 4;
+            }
+            
+            if(s[i] == 'F'){
+                if(dir == 0 || dir == 3) {
+                    xy[dir%2]++;
+                    maxx[dir] = max(maxx[dir],xy[dir%2]);
+                }
+                else {
+                    xy[dir%2]--;
+                    maxx[dir] = min(maxx[dir],xy[dir%2]);
+                }
+            }
+            
+            if(s[i] == 'B'){
+                if(dir == 0 || dir == 3) {
+                    xy[dir%2]--;
+                    maxx[(dir+2)%4] = min(maxx[(dir+2)%4],xy[dir%2]);
+                }
+                else {
+                    xy[dir%2]++;
+                    maxx[(dir+2)%4] = max(maxx[(dir+2)%4],xy[dir%2]);
+                }
+            }
         }
+        
+        //maxx[4] = {0, }; //xmax,ymin,xmin,ymax;
+        
+        int res;
+        res = (maxx[0]-maxx[2]) * (maxx[3]-maxx[1]);
+        
+        cout << res << "\n";
+        
     }
     
-    int num[k];
-    for(int i=0;i<k;i++){
-        cin >> num[i];
-    }
     
-    for(int i=0;i<k;i++){
-        
-        if(num[i] == 1){
-            if(x+1 < m){
-                x++; go(1);
-            }
-        }
-        
-        if(num[i] == 2){
-            if(x != 0){
-                x--; go(2);
-            }
-        }
-        
-        if(num[i] == 3){
-            if(y != 0){
-                y--; go(3);
-            }
-        }
-        
-        if(num[i] == 4){
-            if(y+1 < n){
-                y++; go(4);
-            }
-        }
-        
-        //cout <<"k:" << num[i] << " "<< x << " " << y << " " << MAP[y][x] << "\n";
-        //for(int i=1;i<7;i++) cout << dice[i] << " ";
-        //cout << "\n";
-        
-    }
-    
-    return 0;
 }
