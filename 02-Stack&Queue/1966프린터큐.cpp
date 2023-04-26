b9bc557933e06f8160d0730e239bd0a0f6989d19
ljh0608 <m98m@catholic.ac.kr>
2023-04-26 10:10:15 +0900

    1966_프린터큐

---
+++ 02-Stack&Queue/1966프린터큐.cpp
@@ -0,0 +1,58 @@
+﻿#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int N,M;
+int priority;
+int main() {
+	cin >> N;
+	
+	for (int i = 0; i < N; i++) {
+		priority_queue<int> pq;
+		queue<pair<int, int>> q;
+		cin >> M;
+		int index;
+		cin >> index;
+
+		for (int j = 0; j < M; j++) {
+			cin >> priority;
+			pq.push( priority );
+			q.push({ priority ,j});
+		}
+
+		int num = 1;
+		while (!pq.empty()) {
+
+			if (pq.top() == q.front().first) {
+				if (index == q.front().second) {
+					cout << num << "\n";
+					break;
+				}
+				pq.pop();
+				q.pop();
+				num++;
+			}
+
+			else {
+				int value = q.front().first;
+				int location = q.front().second;
+				q.pop();
+				q.push({ value,location });
+			
+			}
+			
+		}
+
+	}
+	return 0;
+}
+
+
+
