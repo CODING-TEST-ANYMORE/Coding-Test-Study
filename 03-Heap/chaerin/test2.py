10a71b2f98e14f272edbcfb3e189d988f148d74b
chaerin <achrvvv@gmail.com>
2023-04-28 21:02:52 +0900

    solved duplicated product

--- 03-Heap/chaerin/test2.py
+++ 03-Heap/chaerin/test2.py
@@ -1,37 +1,59 @@
-def solution(s):
-    programmer = "programmer"
+# 2. Duplicated Products
+# You are given a complex list of n products, each with a name, price,
+# and weight. Find out how many duplicate products are present
+# within the list. Duplicate products contain identical parameters for
+# all fields in the list (i.e. name, price, and weight).
+# Example:
+# There are n = 5 products with attributes listed in three arrays,
+# aligned by index.
+# name = [ball, bat, glove, glove, glove]
+# price = [2, 3, 1, 2, 1]
+# weight = [2, 5, 1, 1, 1]
+# A complete item description for item 0: (name[0], prices[0], 0:
+# weight[0]) is (ball, 2, 2)
+# Name
+# Price Weight
+# ball
+# 2
+# 2
+# bat
+# 3
+# 5
+# glove
+# 1
+# 1
+# glove
+# 2
+# 1
+# glove
+# 1
+# 1
+# The first two items are unique.
+# The two gloves at indices 2 and 4 are equal in all three attributes so
+# there is 1 duplicate.
+# The last glove at index 3 has a different price from the other two, so
+# it is not a duplicate.
+# There is 1 duplicate item in the original list.
+#
+# Function Description
+# Complete the function numDuplicates in the editor below. The
+# function must return an integer denoting the number of duplicates
+# within the product list.
+# numDuplicates has the following parameter(s):
+# string name[n}. string array of size n, where names[i] denotes the
+# name of the product at the index of i.
+# int price[n): int array of size n, where prices[i] denotes the price of
+# the product at the index of i.
+# int weight[n]. int array of size n, where weights[i] denotes the
+# weight of the product at the index of i.
 
-    head = programmer
-
-    i = 0
-    # programmer
-    for c in s:
-        idx = head.find(c)
-        if idx != -1:
-            head = head[:idx] + head[idx + 1:]
-
-        if len(head) == 0:
-            i += 1
-            break
-        i += 1
-
-    tail = programmer
-
-    j = len(s) - 1
-    # programmer
-    for c in reversed(s):
-        idx = tail.find(c)
-        if idx != -1:
-            tail = tail[:idx] + tail[idx + 1:]
-
-        if len(tail) == 0:
-            j -= 1
-            break
-        j -= 1
-
-    return j - i + 1
+def numDuplicates(name, price, weight):
+    n = len(name)
+    products = [(name[i], price[i], weight[i]) for i in range(n)]
+    unique_products = set(products)
+    return n - len(unique_products)
 
 
 if __name__ == '__main__':
-    result = solution('progxrammerrxproxgrammer')
+    result = numDuplicates(['ball', 'bat', 'glove', 'glove', 'glove'], [2, 3, 1, 2, 1], [2, 5, 1, 1, 1])
     print(result)
