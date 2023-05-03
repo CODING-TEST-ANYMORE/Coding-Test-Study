2a9fd5ab88b5d038a8fb5ab8a1276d2b502914b9
jooyyoo <aliceyun9@naver.com>
2023-05-03 14:26:51 +0900

    [solved] 조합이 불가능한 경우 추가

--- 03-Heap/jooyoung/더 맵게.py
+++ 03-Heap/jooyoung/더 맵게.py
@@ -1,6 +1,6 @@
 def solution(scoville, K):
     answer = 0
-    while True:
+    while len(scoville) > 1:
         answer += 1
 
         first = scoville.pop(scoville.index(min(scoville)))
@@ -10,9 +10,11 @@ def solution(scoville, K):
 
         scoville.append(new_scoville)  # 새로운 스코빌을 배열에 삽입해준다
 
-        scoville.sort() # 배열 오름차순 정렬
+        scoville.sort()  # 배열 오름차순 정렬
 
         if scoville[0] > K:
             break
+    if scoville[0] < K:
+        answer = -1
 
     return answer
