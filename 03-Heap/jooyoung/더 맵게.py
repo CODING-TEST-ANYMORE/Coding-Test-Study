5f98fc05f2e64e806c5266d0334ef1e89e2965c4
jooyyoo <aliceyun9@naver.com>
2023-05-03 13:46:32 +0900

    [solved] for 문 대신 sort 함수로 배열 정렬

--- 03-Heap/jooyoung/더 맵게.py
+++ 03-Heap/jooyoung/더 맵게.py
@@ -2,7 +2,6 @@ def solution(scoville, K):
     answer = 0
     while True:
         answer += 1
-        flag = False
 
         first = scoville.pop(scoville.index(min(scoville)))
         second = scoville.pop(scoville.index(min(scoville)))
@@ -11,13 +10,9 @@ def solution(scoville, K):
 
         scoville.append(new_scoville)  # 새로운 스코빌을 배열에 삽입해준다
 
-        for i in scoville:
-            if i < K:  # K보다 작은 스코빌이 있다면 for문을 나온다
-                break
-            else:
-                flag = True  # 모든 스코빌이 K보다 크다면 통과
+        scoville.sort() # 배열 오름차순 정렬
 
-        if flag:
+        if scoville[0] > K:
             break
 
     return answer
